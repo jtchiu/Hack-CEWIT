146f7edb6875992bee2947288cf97564d549bcc8
jtchiu <justin.chiu@stonybrook.edu>
2017-02-19 05:41:16 -0500

    passed list of recipes to template for dynamic content

--- Hack@CEWIT.py
+++ Hack@CEWIT.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, redirect, url_for
 from pymongo import MongoClient
+from python.Recipe import recipe_from_dict
+import requests
+import json
 
 app = Flask(__name__)
-client = MongoClient('130.245.183.174',80)
-db = client.pantry
-ingredients = db.ingredients
+# client = MongoClient('130.245.183.174',80)
+# db = client.pantry
+# ingredients = db.ingredients
 
 
 @app.route('/')
@@ -13,10 +16,47 @@ def homeHelper():
 
 @app.route('/index')
 def index():
-    return render_template("index.html")
+    client = MongoClient('130.245.183.174', 80)
+    db = client.pantry
+    ingredients = db.ingredients
+
+    ingredients_list = ','.join(ingredient['name'] for ingredient in ingredients.find())
+    number_of_ingredients = len(ingredients_list.split(','))
+    r = requests.post('http://food2fork.com/api/search', data={'key': 'f04d32b283259970d11efb96b6ef215a', 'q': ingredients_list})
+    r = r.content.decode('utf-8')
+    r = json.loads(r)
+
+    r["recipes"] = r["recipes"][:3]
+
+    recipe_list = []
+
+    for recipe in r["recipes"]:
+        r = requests.post('http://food2fork.com/api/get',
+                          data={'key': 'f04d32b283259970d11efb96b6ef215a', 'rId': recipe['recipe_id']})
+        r = r.content.decode('utf-8')
+        r = json.loads(r)
+        r = r['recipe']
+        recipe_dict = {
+            'f2f_url': r['f2f_url'],
+            'ingredients': r['ingredients'],
+            'recipe_id': r['recipe_id'],
+            'image_url': r['image_url'],
+            'num_items': len(r['ingredients']),
+            'percentage' : int(((number_of_ingredients/len(r['ingredients']))*100)),
+            'title': r['title']
+        }
+        recipe_list.append(recipe_dict)
+
+    recipe_list.sort(key=lambda x: x['num_items'])
+    # recipes = [recipe_from_dict(x) for x in recipe_list]
+    return render_template("index.html", recipe_list=recipe_list)
 
 @app.route('/search')
 def search():
+    client = MongoClient('130.245.183.174', 80)
+    db = client.pantry
+    ingredients = db.ingredients
+
     ingredients_list = [ingredient['name'] for ingredient in ingredients.find()]
 
     return render_template("search.html",ingredients_list=ingredients_list)
--- python/Recipe.py
+++ python/Recipe.py
@@ -1,11 +1,11 @@
 
 class Recipe():
-    def __init__(self, name, id, img_url, f2f_url):
-        self = name
-        self = id
-        self = img_url
+    def __init__(self, title, recipe_id, image_url, f2f_url):
+        self = title
+        self = recipe_id
+        self = image_url
         self = f2f_url
 
-    @staticmethod
-    def recipe_from_dict(dict):
-        return Recipe()
+
+def recipe_from_dict(dict):
+    return Recipe(dict['title'], dict['recipe_id'], dict['image_url'], dict['f2f_url'])
