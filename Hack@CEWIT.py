b3e54cc63370c10b4a6b3e33633331bd78517f84
Justin Chiu <justin.chiu@stonybrook.edu>
2017-02-19 01:46:06 -0500

    flask set up

--- Hack@CEWIT.py
+++ Hack@CEWIT.py
@@ -1,12 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return render_template("home.html")
+def homeHelper():
+    return redirect(url_for('home'))
 
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+@app.route('/search')
+def search():
+
+    return render_template("search.html")
+
+@app.route('/help')
+def help():
+    return render_template("help.html")
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
 
 #@app.route('/')
 
