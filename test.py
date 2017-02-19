b3a44bfb0b0280ec346f1a96e5bed809aca9eb39
Justin Chiu <justin.chiu@stonybrook.edu>
2017-02-19 01:44:11 -0500

    front end set-up 100%

---
+++ test.py
@@ -0,0 +1,16 @@
+
+
+
+if __name__ == '__main__':
+    list = ["Baking powder","Baking soda","Brown sugar",
+            "Cornstarch","All-purpose flour","Granulated sugar","Honey","Butter",
+            "Cheddar cheese", "Feta", "Parmesan", "Mozzarella","Eggs","Milk",
+            "Plain yogurt","Tortillas","Garlic","Onions","Potatoes","Raisins",
+             "Apricots","Almonds", "Peanuts", "Sunflower","Breadcrumbs",
+             "Pasta","Rice","Rolled oats","Barley", "Millet", "Quinoa", "Wheatberries",
+             "Black beans", "Cannellini beans", "Chickpeas", "Kidney beans","Capers",
+             "Olives","Peanut butter","Jelly","Stock/Broth","Tomatoes","Salsa",
+             "Tuna fish"]
+    list.sort()
+
+    print(*list, sep="\n")
